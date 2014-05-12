154109a6ae11b3d667765d89fbe88c2663d8f063
Mateusz Uzdowski <mateusz@silverstripe.com>
2014-05-12 15:32:34 +1200

    Make vary header adjustable, update docs.

--- code/policies/CachingPolicy.php
+++ code/policies/CachingPolicy.php
@@ -9,20 +9,28 @@
  *     class: CachingPolicy
  *     properties:
  *       cacheAge: 300
- *
  * HomePage_Controller:
- *   extensions:
- *     - ControllerPolicyApplicator('CachingPolicy')
+ *   dependencies:
+ *     Policies: '%$CachingPolicy'
  */
-
 class CachingPolicy implements ControllerPolicy {
 
+	/**
+	 * @var int $cacheAge Max-age seconds to cache for.
+	 */
 	public $cacheAge = 0;
+
+	/**
+	 * @var string $vary Vary string to add. Do not add user-agent unless you vary on it and you have configured
+	 *	user-agent clustering in some way, otherwise this will be an equivalent to disabling caching as there
+	 *	is a lot of different UAs in the wild.
+	 */
+	public $vary = 'Cookie, X-Forwarded-Protocol, Accept';
 
 	public function applyToResponse(SS_HTTPRequest $request, SS_HTTPResponse $response, DataModel $model) {
 		$response->addHeader('Cache-Control', "max-age=" . $this->cacheAge . ", must-revalidate, no-transform");
 		$response->addHeader('Pragma', '');
-		$response->addHeader('Vary', 'Cookie, X-Forwarded-Protocol, Accept');
+		$response->addHeader('Vary', $this->vary);
 	}
 
 }
